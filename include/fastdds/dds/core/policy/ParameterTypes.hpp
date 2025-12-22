159f3b4834c5673cc10fc1d27411d188997d8fbe
Miguel Company <MiguelCompany@eprosima.com>
2025-12-22 15:55:36 +0100

    Rebase from 'rpm/humble/fastrtps'

--- include/fastdds/dds/core/policy/ParameterTypes.hpp
+++ include/fastdds/dds/core/policy/ParameterTypes.hpp
@@ -23,7 +23,6 @@
 
 #include <fastdds/rtps/common/all_common.h>
 #include <fastdds/rtps/common/Token.h>
-#include <fastdds/rtps/messages/CDRMessage.h>
 
 #include <fastrtps/utils/fixed_size_string.hpp>
 
@@ -153,6 +152,9 @@ enum ParameterId_t : uint16_t
     PID_RELATED_SAMPLE_IDENTITY             = 0x800f,
     PID_DISABLE_POSITIVE_ACKS               = 0x8005,
     PID_DATASHARING                         = 0x8006,
+
+    /* eProsima Safe DDS extensions */
+    PID_SAFE_DDS_SIGNATURE                  = 0x9000,
 };
 
 /*!
